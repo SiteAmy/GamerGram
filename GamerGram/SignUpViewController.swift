afa98316a68982f1c9fcf8d636e0a8539cefe388
Ruben Nijmeijer <r.nijmeijer.gamedev@gmail.com>
2019-04-26 12:06:18 +0200

    started on tab bars added resources

---
+++ GamerGram/SignUpViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignUpViewController.swift
+//  GamerGram
+//
+//  Created by Ruben Nijmeijer on 26/04/2019.
+//  Copyright © 2019 Ruben Nijmeijer. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissOnClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
