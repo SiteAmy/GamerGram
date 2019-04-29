0f05f4ec8cd09a248130e4f57b131bf259482b74
Ruben Nijmeijer <r.nijmeijer.gamedev@gmail.com>
2019-04-29 22:04:27 +0200

    Coded the login and signing view UI

---
+++ GamerGram/Viewcontrollers/SignUpViewController.swift
@@ -0,0 +1,69 @@
+//
+//  SignUpViewController.swift
+//  GamerGram
+//
+//  Created by Ruben Nijmeijer on 26/04/2019.
+//  Copyright © 2019 Ruben Nijmeijer. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var profileImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        usernameTextField.backgroundColor = UIColor.clear
+        usernameTextField.tintColor = UIColor.white
+        usernameTextField.textColor = UIColor.white
+        usernameTextField.attributedPlaceholder =
+            NSAttributedString(string: usernameTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: UIColor(white:1.0, alpha: 0.6)])
+        let bottomLayerUserName = CALayer()
+        bottomLayerUserName.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerUserName.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        usernameTextField.layer.addSublayer(bottomLayerUserName)
+        
+        passwordTextField.backgroundColor = UIColor.clear
+        passwordTextField.tintColor = UIColor.white
+        passwordTextField.textColor = UIColor.white
+        passwordTextField.attributedPlaceholder =
+            NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: UIColor(white:1.0, alpha: 0.6)])
+        let bottomLayerPassword = CALayer()
+        bottomLayerPassword.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerPassword.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        passwordTextField.layer.addSublayer(bottomLayerPassword)
+        
+        emailTextField.backgroundColor = UIColor.clear
+        emailTextField.tintColor = UIColor.white
+        emailTextField.textColor = UIColor.white
+        emailTextField.attributedPlaceholder =
+            NSAttributedString(string: emailTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: UIColor(white:1.0, alpha: 0.6)])
+        let bottomLayerEmail = CALayer()
+        bottomLayerEmail.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerEmail.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        emailTextField.layer.addSublayer(bottomLayerEmail)
+        
+        profileImage.layer.cornerRadius = 40
+        profileImage.clipsToBounds = true
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissOnClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
